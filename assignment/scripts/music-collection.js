29e425361715d65ec4878564b21611721ea08f34
Kash <kserenitym@gmail.com>
2021-06-03 14:25:23 -0500

    working on show collection

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,35 +1,58 @@
 console.log('***** Music Collection *****')
-var collection= [];
+var collection = [];
 
-function addToCollection(title, artist, year){
-  let album={
+
+function addToCollection(title, artist, year) {
+  collection.push({
+    title,
+    artist,
+    year
+  });
+  let album = {
     title: title,
     artist: artist,
     year: year,
   };
-  collection.push({title,artist,year});
   return album;
 } //end addCollection
 
-console.log(addToCollection('Sweetener','Ariana Grande',2018));
-console.log(addToCollection('WELCOME HOME','Aries',2019));
-console.log(addToCollection('THE REAPER','Keshi',2018));
-console.log(addToCollection('Skeletons','Keshi',2019));
-console.log(addToCollection('Zephyr','NIKI',2018));
-console.log(addToCollection('Some boring love stories pt 5','Powfu',2020));
+//testing addToCollection
+console.log(addToCollection('Sweetener', 'Ariana Grande', 2018));
+console.log(addToCollection('WELCOME HOME', 'Aries', 2019));
+console.log(addToCollection('THE REAPER', 'Keshi', 2018));
+console.log(addToCollection('Skeletons', 'Keshi', 2019));
+console.log(addToCollection('Zephyr', 'NIKI', 2018));
+console.log(addToCollection('Some boring love stories pt 5', 'Powfu', 2020));
+console.log(collection)
 
-console.log(collection);
 
-function showCollection(collection){
-  console.log(collection.length);
-  for(let i=0; i<collection.length; i++){
-    return collection;
-  }
-  console.log(collection)
-} //end showCollection
 
-console.log('Here is some of my favorites:');
+function showCollection(collection) {
+  console.log(`There are ${collection.length} albums in this collection`);
+  let album = {
+    title: title,
+    artist: artist,
+    year: year,
+  };
+  for (let i = 0; i < collection.length; i++);
+  console.log(`${album.title} by ${album.artist} published in ${album.year}.`);
 
-function findByArtist(artist){
+  return collection;
+}
+//end showCollection
 
-}//end findByArtist
+//testing showCollection
+console.log('Here is a collection of my albums:', showCollection(collection))
+
+
+
+
+
+function findByArtist(artist) {
+
+  return artist;
+} //end findByArtist
+
+
+
+//testing findByArtist
