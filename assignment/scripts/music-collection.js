b26ba1919c27a72209c6479981b642296e49126b
Kash <kserenitym@gmail.com>
2021-06-03 00:12:07 -0500

    work in progress

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,7 +1,26 @@
 console.log('***** Music Collection *****')
 var collection= [];
 
-function addCollection(title, artist, year){
+function addToCollection(title, artist, year){
+  let album={
+    title: title,
+    artist: artist,
+    year: year,
+  };
   collection.push({title,artist,year});
-  return{title,artist,year};
+  return album;
+} //end addCollection
+
+console.log(addToCollection('Sweetener','Ariana Grande',2018));
+console.log(addToCollection('WELCOME HOME','Aries',2019));
+console.log(addToCollection('THE REAPER','Keshi',2018));
+console.log(addToCollection('Skeletons','Keshi',2019));
+console.log(addToCollection('Zephyr','NIKI',2018));
+console.log(addToCollection('Some boring love stories pt 5','Powfu',2020));
+
+console.log(collection);
+
+function showCollection(album){
+
+  console.log(collection.length);
 }
